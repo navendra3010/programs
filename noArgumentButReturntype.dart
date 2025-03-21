f38ad01dcb876bd69abd990ca6b90c672916da49
Navendra sahu <sahunavendra12gmail.com>
2025-03-21 18:09:22 +0530

    new commit

--- noArgumentButReturntype.dart
+++ noArgumentButReturntype.dart
@@ -1,15 +1,14 @@
 import 'dart:ffi';
 
-void main()
-{
-  int price=myprice();
-print(price);
+void main() {
+  int price = myprice();
+  print(price);
+  print("hello");
 }
-int myprice()
-{
-  int  p=1;
-  int  r=2;
-  int c=p+r;
-  return c;
 
+int myprice() {
+  int p = 1;
+  int r = 2;
+  int c = p + r;
+  return c;
 }
